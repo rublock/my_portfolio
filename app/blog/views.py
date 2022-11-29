1f702d95bdd6ab65f7564890f89255fd06638327
Mack <hypermail@yandex.ru>
2022-11-29 20:06:27 +0300

    main_boostrap_5_portfolio_front

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
+
 
 def all_blogs(request):
     blog = Blog.objects.all()
     return render(request, 'blog/all_blogs.html', {'blog': blog})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'id': blog})
