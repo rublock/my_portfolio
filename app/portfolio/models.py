5c41c7a727ba10eb785704e1acd6b4f2dc6ff059
Mack <hypermail@yandex.ru>
2022-11-30 17:39:00 +0300

    main_tech_front

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=999)
+    description = models.TextField(max_length=9999)
     image = models.ImageField(upload_to='portfolio/img')
-    url = models.URLField(blank=True) #open in new page
+    url = models.URLField(blank=True)  # open in new page
+    tech = models.JSONField()
